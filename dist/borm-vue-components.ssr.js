d45af83b24259ead6ad057129f1f5b03e3a082cd
Lackner_Thomas <thomas.lackner@borm.ch>
2021-12-13 12:51:06 +0100

    initial commit, added first artifacts for testing

---
+++ dist/borm-vue-components.ssr.js
@@ -0,0 +1,223 @@
+'use strict';var script = {
+  name: 'BormVueComponentsSample',
+
+  // vue component name
+  data() {
+    return {
+      counter: 5,
+      initCounter: 5,
+      message: {
+        action: null,
+        amount: null
+      }
+    };
+  },
+
+  computed: {
+    changedBy() {
+      const {
+        message
+      } = this;
+      if (!message.action) return 'initialized';
+      return `${message.action} ${message.amount || ''}`.trim();
+    }
+
+  },
+  methods: {
+    increment(arg) {
+      const amount = typeof arg !== 'number' ? 1 : arg;
+      this.counter += amount;
+      this.message.action = 'incremented by';
+      this.message.amount = amount;
+    },
+
+    decrement(arg) {
+      const amount = typeof arg !== 'number' ? 1 : arg;
+      this.counter -= amount;
+      this.message.action = 'decremented by';
+      this.message.amount = amount;
+    },
+
+    reset() {
+      this.counter = this.initCounter;
+      this.message.action = 'reset';
+      this.message.amount = null;
+    }
+
+  }
+};function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier /* server only */, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+    if (typeof shadowMode !== 'boolean') {
+        createInjectorSSR = createInjector;
+        createInjector = shadowMode;
+        shadowMode = false;
+    }
+    // Vue.extend constructor export interop.
+    const options = typeof script === 'function' ? script.options : script;
+    // render functions
+    if (template && template.render) {
+        options.render = template.render;
+        options.staticRenderFns = template.staticRenderFns;
+        options._compiled = true;
+        // functional template
+        if (isFunctionalTemplate) {
+            options.functional = true;
+        }
+    }
+    // scopedId
+    if (scopeId) {
+        options._scopeId = scopeId;
+    }
+    let hook;
+    if (moduleIdentifier) {
+        // server build
+        hook = function (context) {
+            // 2.3 injection
+            context =
+                context || // cached call
+                    (this.$vnode && this.$vnode.ssrContext) || // stateful
+                    (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
+            // 2.2 with runInNewContext: true
+            if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+                context = __VUE_SSR_CONTEXT__;
+            }
+            // inject component styles
+            if (style) {
+                style.call(this, createInjectorSSR(context));
+            }
+            // register component module identifier for async chunk inference
+            if (context && context._registeredComponents) {
+                context._registeredComponents.add(moduleIdentifier);
+            }
+        };
+        // used by ssr in case component is cached and beforeCreate
+        // never gets called
+        options._ssrRegister = hook;
+    }
+    else if (style) {
+        hook = shadowMode
+            ? function (context) {
+                style.call(this, createInjectorShadow(context, this.$root.$options.shadowRoot));
+            }
+            : function (context) {
+                style.call(this, createInjector(context));
+            };
+    }
+    if (hook) {
+        if (options.functional) {
+            // register for functional component in vue file
+            const originalRender = options.render;
+            options.render = function renderWithStyleInjection(h, context) {
+                hook.call(context);
+                return originalRender(h, context);
+            };
+        }
+        else {
+            // inject component registration as beforeCreate hook
+            const existing = options.beforeCreate;
+            options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+        }
+    }
+    return script;
+}function createInjectorSSR(context) {
+    if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+        context = __VUE_SSR_CONTEXT__;
+    }
+    if (!context)
+        return () => { };
+    if (!('styles' in context)) {
+        context._styles = context._styles || {};
+        Object.defineProperty(context, 'styles', {
+            enumerable: true,
+            get: () => context._renderStyles(context._styles)
+        });
+        context._renderStyles = context._renderStyles || renderStyles;
+    }
+    return (id, style) => addStyle(id, style, context);
+}
+function addStyle(id, css, context) {
+    const group = css.media || 'default' ;
+    const style = context._styles[group] || (context._styles[group] = { ids: [], css: '' });
+    if (!style.ids.includes(id)) {
+        style.media = css.media;
+        style.ids.push(id);
+        let code = css.source;
+        style.css += code + '\n';
+    }
+}
+function renderStyles(styles) {
+    let css = '';
+    for (const key in styles) {
+        const style = styles[key];
+        css +=
+            '<style data-vue-ssr-id="' +
+                Array.from(style.ids).join(' ') +
+                '"' +
+                (style.media ? ' media="' + style.media + '"' : '') +
+                '>' +
+                style.css +
+                '</style>';
+    }
+    return css;
+}/* script */
+const __vue_script__ = script;
+/* template */
+
+var __vue_render__ = function () {
+  var _vm = this;
+
+  var _h = _vm.$createElement;
+
+  var _c = _vm._self._c || _h;
+
+  return _c('div', {
+    staticClass: "borm-vue-components-sample"
+  }, [_vm._ssrNode("<p data-v-21102f7b>" + _vm._ssrEscape("The counter was " + _vm._s(_vm.changedBy) + " to ") + "<b data-v-21102f7b>" + _vm._ssrEscape(_vm._s(_vm.counter)) + "</b>.</p> <button data-v-21102f7b>\n        Click +1\n    </button> <button data-v-21102f7b>\n        Click -1\n    </button> <button data-v-21102f7b>\n        Click +5\n    </button> <button data-v-21102f7b>\n        Click -5\n    </button> <button data-v-21102f7b>\n        Reset\n    </button>")]);
+};
+
+var __vue_staticRenderFns__ = [];
+/* style */
+
+const __vue_inject_styles__ = function (inject) {
+  if (!inject) return;
+  inject("data-v-21102f7b_0", {
+    source: ".borm-vue-components-sample[data-v-21102f7b]{display:block;width:400px;margin:25px auto;border:1px solid #ccc;background:#eaeaea;text-align:center;padding:25px}.borm-vue-components-sample p[data-v-21102f7b]{margin:0 0 1em}",
+    map: undefined,
+    media: undefined
+  });
+};
+/* scoped */
+
+
+const __vue_scope_id__ = "data-v-21102f7b";
+/* module identifier */
+
+const __vue_module_identifier__ = "data-v-21102f7b";
+/* functional template */
+
+const __vue_is_functional_template__ = false;
+/* style inject shadow dom */
+
+const __vue_component__ = /*#__PURE__*/normalizeComponent({
+  render: __vue_render__,
+  staticRenderFns: __vue_staticRenderFns__
+}, __vue_inject_styles__, __vue_script__, __vue_scope_id__, __vue_is_functional_template__, __vue_module_identifier__, false, undefined, createInjectorSSR, undefined);
+
+var __vue_component__$1 = __vue_component__;var components$1=/*#__PURE__*/Object.freeze({__proto__:null,BormVueComponentsSample:__vue_component__$1});// Import vue components
+
+const install = function installBormVueComponents(Vue) {
+  Object.entries(components$1).forEach(_ref => {
+    let [componentName, component] = _ref;
+    Vue.component(componentName, component);
+  });
+}; // Create module definition for Vue.use()
+var components=/*#__PURE__*/Object.freeze({__proto__:null,'default':install,BormVueComponentsSample:__vue_component__$1});// iife/cjs usage extends esm default export - so import it all
+// only expose one global var, with component exports exposed as properties of
+// that global var (eg. plugin.component)
+
+Object.entries(components).forEach(_ref => {
+  let [componentName, component] = _ref;
+
+  if (componentName !== 'default') {
+    install[componentName] = component;
+  }
+});module.exports=install;
